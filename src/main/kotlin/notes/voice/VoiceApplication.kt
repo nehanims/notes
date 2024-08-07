341be9462c1bff59839438a8f927b15a2e564c6f
nehanims <neha.h.nims@gmail.com>
2024-08-07 13:56:31 -0700

    - added TODO for later

--- src/main/kotlin/notes/voice/VoiceApplication.kt
+++ src/main/kotlin/notes/voice/VoiceApplication.kt
@@ -2,7 +2,7 @@ package notes.voice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
+//TODO : Go through the Spring Kotlin docs to see what changes need to be made for things to work with kotlin https://spring.io/guides/tutorials/spring-boot-kotlin
 @SpringBootApplication
 class VoiceApplication
 
